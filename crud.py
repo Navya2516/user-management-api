22e173d7bbea1243935eff3f0a7dbbbec8745145
Navya2516 <navyasaravanan16@gmail.com>
2026-03-27 14:03:24 +0530

    Initial commit - User Management API using FastAPI and PostgreSQL

---
+++ crud.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+import models, schemas
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(**user.dict())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_users(db: Session):
+    return db.query(models.User).all()
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def update_user(db: Session, user_id: int, user: schemas.UserCreate):
+    db_user = get_user(db, user_id)
+    if db_user:
+        for key, value in user.dict().items():
+            setattr(db_user, key, value)
+        db.commit()
+        db.refresh(db_user)
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    db_user = get_user(db, user_id)
+    if db_user:
+        db.delete(db_user)
+        db.commit()
+    return db_user
